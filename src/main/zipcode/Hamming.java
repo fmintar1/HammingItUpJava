3e25bcb0f0c4710fa1563a940581e5084a970369
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-11-29 11:32:20 -0500

    Done

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,24 @@
-package zipcode;
+package src.main.zipcode;
 
 public class Hamming {
+    String s;
+    String s1;
     public Hamming(String s, String s1) {
+        if(s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        this.s = s;
+        this.s1 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+        int hammingCounter = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != s1.charAt(i)) {
+                    hammingCounter++;
+
+            }
+        }
+        return hammingCounter;
     }
 }
